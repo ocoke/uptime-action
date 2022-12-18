68fbc02da09f03129fe2f331e4353441bb200dc1
CKY <awa@outlook.ie>
2022-12-18 04:03:32 +0000

    :tada: First Commit

---
+++ _config.js
@@ -0,0 +1,10 @@
+export default {
+    list: [
+       { host: "yfun.top", ssl: true, port: 443, name: "YFun's Home", method: "GET", ok: "200-299" },
+       { host: "blog.yfun.top", ssl: true, port: 443, name: "YFun's Blog", method: "GET", ok: "200-299" },
+    ],
+    filedir: "./data/",
+    filename: "{{@date}}.json",
+    limit: 3,
+    node: "Action"
+}
